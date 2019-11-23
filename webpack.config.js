09983ae7287bbcd748a0e490c7c4697974effe76
Chuntong Gao <heihegao@gmail.com>
2019-11-22 21:57:37 -0800

    add webpack.config.js, finish webpack config

---
+++ webpack.config.js
@@ -0,0 +1,15 @@
+/* eslint-disable global-require */
+switch (process.env.NODE_ENV) {
+    case 'production':
+        module.exports = require('./config/production.webpack.config');
+        break;
+    case 'chrome':
+        module.exports = require('./config/chrome.webpack.config');
+        break;
+    case 'analysis':
+        module.exports = require('./config/analysis.webpack.config');
+        break;
+    default:
+        module.exports = require('./config/development.webpack.config');
+        break;
+}
