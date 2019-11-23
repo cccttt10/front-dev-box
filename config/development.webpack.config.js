ceec5ec678aedf2e989d5409650d38abf75da664
Chuntong Gao <heihegao@gmail.com>
2019-11-22 21:52:08 -0800

    add development.webpack.config.js

---
+++ config/development.webpack.config.js
@@ -0,0 +1,20 @@
+const webpack = require('webpack');
+const { smart } = require('webpack-merge');
+
+const config = require('./base.webpack.config');
+
+module.exports = smart(config, {
+    mode: 'development',
+    entry: {
+        main: './src/main.js'
+    },
+    devServer: {
+        hot: true,
+        host: '0.0.0.0'
+    },
+    plugins: [ new webpack.HotModuleReplacementPlugin() ],
+    externals: {
+        vue: 'Vue',
+        'element-ui': 'ELEMENT'
+    }
+});
