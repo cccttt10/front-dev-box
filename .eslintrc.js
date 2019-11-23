59e7d5a2e02b2ca7b541d445aef93a05fb975a2d
Chuntong Gao <heihegao@gmail.com>
2019-11-22 21:13:16 -0800

    add .eslintrc.js

---
+++ .eslintrc.js
@@ -0,0 +1,17 @@
+module.exports = {
+    env: {
+      browser: true,
+      commonjs: true,
+      node: true,
+      es6: true,
+      mocha: true
+    },
+    extends: ['eslint-config-airbnb-base', 'plugin:vue/essential'],
+    parserOptions: {
+      ecmaVersion: 2018,
+      sourceType: 'module'
+    },
+    rules: {
+      'import/no-unresolved': 'off'
+    }
+};
