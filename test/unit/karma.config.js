eedce3b6a2082d0d783f6cebbfff06bfd5dd79c0
Chuntong Gao <heihegao@gmail.com>
2019-11-24 10:38:25 -0800

    finish karma config

---
+++ test/unit/karma.config.js
@@ -0,0 +1,24 @@
+const path = require('path');
+const webpackConfig = require('../../config/base.webpack.config.js');
+
+webpackConfig.devtool = 'inline-source-map';
+
+module.exports = config => {
+    config.set({
+        basePath: '',
+        frameworks: ['mocha'],
+        files: ['./index.js'],
+        preprocessors: {
+            'index.js': ['webpack', 'sourcemap']
+        },
+        reporters: ['spec', 'coverage'],
+        coverageReporter: {
+            dir: path.join(__dirname, './coverage'),
+            reporters: [{ type: 'lcov', subdir: '.' }, { type: 'text-summary' }]
+        },
+        colors: true,
+        browsers: ['ChromeHeadless'],
+        singleRun: true,
+        webpack: webpackConfig
+    });
+};
