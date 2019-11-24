8d30e098c0a86ff1430ebfce64d975b8e0a2ad3e
Chuntong Gao <heihegao@gmail.com>
2019-11-23 16:55:21 -0800

    add Main.vue and db.js

---
+++ src/db/db.js
@@ -0,0 +1,238 @@
+export const searchOptions = [
+    {
+        label: 'Google',
+        value: 0,
+        url: 'https://www.google.com/search?q='
+    },
+    {
+        label: 'GitHub',
+        value: 1,
+        url: 'https://github.com/search?q='
+    },
+    {
+        label: 'NPM Package',
+        value: 2,
+        url: 'https://www.npmjs.com/search?q='
+    },
+    {
+        label: 'Stack Overflow',
+        value: 3,
+        url: 'https://stackoverflow.com/search?q='
+    },
+    {
+        label: 'MDN',
+        value: 4,
+        url: 'https://developer.mozilla.org/en-US/search?q='
+    },
+    {
+        label: 'Medium',
+        value: 5,
+        url: 'https://medium.com/search?q='
+    }
+];
+
+export const toolList = {
+    title: 'tools & references',
+    list: [
+        {
+            name: 'Regex Reference',
+            url: 'https://github.com/ziishaned/learn-regex'
+        },
+        {
+            name: 'Regex Playground',
+            url: 'https://regex101.com/r/tUxrBG/1'
+        },
+        {
+            name: 'Flow Chart Generator',
+            url: 'https://www.processon.com'
+        },
+        {
+            name: 'Fake JSON',
+            url: 'https://www.json-generator.com'
+        },
+        {
+            name: 'JavaScript REPL',
+            url: 'https://repl.it/languages/JavaScript'
+        },
+        {
+            name: 'CodeSandbox',
+            url: 'https://codesandbox.io'
+        },
+        {
+            name: 'Mobile Device Debugger',
+            url: 'https://github.com/wuchangming/spy-debugger/blob/master/README_EN.md'
+        },
+        {
+            name: 'CDN',
+            url: 'https://cdnjs.com'
+        },
+        {
+            name: 'Site Performance',
+            url: 'https://web.dev/measure/'
+        },
+        {
+            name: 'Google Analytics',
+            url: 'https://analytics.google.com/'
+        },
+        {
+            name: 'Google Domains',
+            url: 'https://domains.google'
+        },
+        {
+            name: 'NPM Bundle Size',
+            url: 'https://bundlephobia.com'
+        },
+        {
+            name: 'JavaScript Keycodes',
+            url: 'http://keycodes.atjayjo.com'
+        },
+        {
+            name: 'Linux Commands',
+            url: 'https://tldr.ostera.io'
+        },
+        {
+            name: 'Linux 命令',
+            url: 'https://wangchujiang.com/linux-command/'
+        },
+        {
+            name: 'Docusaurus: Doc Generator',
+            url: 'https://docusaurus.io'
+        },
+        {
+            name: 'CSS Reference',
+            url: 'https://tympanus.net/codrops/css_reference/'
+        },
+        {
+            name: 'CSS Visual Reference',
+            url: 'https://cssreference.io'
+        },
+        {
+            name: 'Google Fonts',
+            url: 'https://fonts.google.com'
+        }
+    ]
+};
+
+export const specList = [
+    {
+        name: 'Semantic Versioning',
+        url: 'https://semver.org'
+    },
+    {
+        name: 'Licenses',
+        url: 'https://choosealicense.com/licenses/'
+    },
+    {
+        name: 'JSON Schema',
+        url: 'https://www.json.org/json-zh.html'
+    },
+    {
+        name: 'ES5 Spec',
+        url: 'http://es5.github.io'
+    },
+    {
+        name: 'Airbnb JavaScript Style Guide',
+        url: 'https://github.com/airbnb/javascript'
+    }
+];
+
+export const learningList = [
+    {
+        name: '2019 Front-End Handbook',
+        url: 'https://frontendmasters.com/books/front-end-handbook/2019/'
+    },
+    {
+        name: '2019 Developer Roadmap',
+        url: 'https://github.com/kamranahmedse/developer-roadmap'
+    },
+    {
+        name: 'Leet Code',
+        url: 'https://leetcode.com'
+    },
+    {
+        name: 'Design Patterns',
+        url: 'https://refactoring.guru/design-patterns/catalog'
+    },
+    {
+        name: 'Udemy',
+        url: 'https://www.udemy.com'
+    }
+];
+
+export const techList = [
+    {
+        name: 'React',
+        url: 'https://reactjs.org'
+    },
+    {
+        name: 'React Lifecycle Methods',
+        url: 'http://projects.wojtekmaj.pl/react-lifecycle-methods-diagram/'
+    },
+    {
+        name: 'Redux',
+        url: 'https://redux.js.org'
+    },
+    {
+        name: 'Vue',
+        url: 'https://vuejs.org/index.html'
+    },
+    {
+        name: 'Angular',
+        url: 'https://angular.io'
+    },
+    {
+        name: 'Webpack',
+        url: 'https://webpack.js.org/'
+    },
+    {
+        name: 'Node',
+        url: 'https://nodejs.org/en/'
+    },
+    {
+        name: 'Express',
+        url: 'https://expressjs.com'
+    },
+    {
+        name: 'TypeScript',
+        url: 'https://www.typescriptlang.org'
+    },
+    {
+        name: 'GraphQL',
+        url: 'https://graphql.org'
+    },
+    {
+        name: 'Babel',
+        url: 'https://babeljs.io'
+    },
+    {
+        name: 'Docker',
+        url: 'https://docs.docker.com/'
+    },
+    {
+        name: 'Sass',
+        url: 'https://sass-lang.com'
+    },
+    {
+        name: 'Netlify',
+        url: 'https://www.netlify.com'
+    },
+    {
+        name: 'ESLint',
+        url: 'https://eslint.org'
+    },
+    {
+        name: 'Prettier',
+        url: 'https://prettier.io'
+    }
+];
+
+export const blogList = [
+    {
+        name: 'Dan Abramov',
+        url: 'https://overreacted.io'
+    },
+    {
+        name: 'Philip Roberts',
+        url: 'https://github.com/latentflip'
+    }
+];
